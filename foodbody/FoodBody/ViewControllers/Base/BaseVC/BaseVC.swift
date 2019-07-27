bb930d672a465a349ff178e766d38c55eb640074
Phuoc <kieuvanphuoc277@gmail.com>
2019-07-27 21:21:35 +0700

    Add edges for extend layout to flexible with ios 10.3

--- foodbody/FoodBody/ViewControllers/Base/BaseVC/BaseVC.swift
+++ foodbody/FoodBody/ViewControllers/Base/BaseVC/BaseVC.swift
@@ -17,6 +17,7 @@ class BaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		setupNavigationBarItem()
+        self.edgesForExtendedLayout = UIRectEdge.init()
 		
 
     }
