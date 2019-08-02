735d5641c6be27b2699fc97a237f08649a51128f
Toan Tran <toan.tran@softfoundry.com>
2019-08-02 18:03:00 +0700

    update reservation tab

---
+++ foodbody/FoodBody/ViewControllers/Reservation/ReservationVC/ReservationCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReservationCell.swift
+//  FoodBody
+//
+//  Created by Toan Tran on 8/2/19.
+//  Copyright © 2019 KPT. All rights reserved.
+//
+
+import UIKit
+
+class ReservationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- foodbody/FoodBody/ViewControllers/Reservation/ReservationVC/ReservationVC.swift
+++ foodbody/FoodBody/ViewControllers/Reservation/ReservationVC/ReservationVC.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class ReservationVC: UIViewController {
-
+    @IBOutlet var tbvReservation:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tbvReservation.delegate = self;
+        self.tbvReservation.dataSource = self;
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension ReservationVC: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:ReservationCell = tableView.dequeueReusableCell(withIdentifier: "ReservationCell", for: indexPath) as! ReservationCell;
+        return cell;
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120;
+    }
+}
+
+
