4d1739ceccb24ad3f18e5eb71360d2137a6ffd52
Vuong Toan Chung <chungvtgc00989@gmail.com>
2019-08-09 17:34:53 +0700

    Add chart in profile

--- foodbody/FoodBody/ViewControllers/Profile/ProfileVC/ProfileVC.swift
+++ foodbody/FoodBody/ViewControllers/Profile/ProfileVC/ProfileVC.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import Charts
 
 
 class ProfileVC: UIViewController {
     
     @IBOutlet weak var stepView: UIView!
+	@IBOutlet var chartView: PieChartView!
+	
+	var rateDataSource: [Int] =  [70, 30]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+		setupChart()
     }
     
     
@@ -23,12 +28,81 @@ class ProfileVC: UIViewController {
        stepView.layer.borderColor = Style.Color.showDowColor.cgColor
        stepView.layer.borderWidth = 1
     }
+	
+	private func setupChart() {
+	
+		let l = chartView.legend
+		l.horizontalAlignment = .right
+		l.verticalAlignment = .top
+		l.orientation = .vertical
+		l.xEntrySpace = 7
+		l.yEntrySpace = 0
+		l.yOffset = 0
+		
+		chartView.entryLabelColor = .white
+		chartView.entryLabelFont = .systemFont(ofSize: 12, weight: .light)
+		chartView.usePercentValuesEnabled = false
+		chartView.drawSlicesUnderHoleEnabled = false
+		chartView.holeRadiusPercent = 0.8//0.58
+		chartView.transparentCircleRadiusPercent = 0.61
+		chartView.chartDescription?.enabled = false
+		chartView.setExtraOffsets(left: 5, top: 10, right: 5, bottom: 5)
+		chartView.drawCenterTextEnabled = true
+		chartView.centerText = "500Kcal";
+		chartView.drawHoleEnabled = true
+		chartView.rotationAngle = 0
+		chartView.rotationEnabled = false
+		chartView.highlightPerTapEnabled = false
+		
+		let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+		paragraphStyle.lineBreakMode = .byTruncatingTail
+		paragraphStyle.alignment = .center
+		
+		chartView.animate(xAxisDuration: 0.4, easingOption: .easeInCirc)
+		self.updateChartData()
+	}
     
 
     @IBAction func logoutPress(sender:UIButton){
         AppManager.user = nil
         FBAppDelegate.gotoWelcome()
     }
+	
+	func updateChartData() {
+		self.setDataCount(Int(2), range: 100)
+	}
+	
+	func setDataCount(_ count: Int, range: UInt32) {
+		let entries = (0..<count).map { (i) -> PieChartDataEntry in
+			// IMPORTANT: In a PieChart, no values (Entry) should have the same xIndex (even if from different DataSets), since no values can be drawn above each other.
+			return PieChartDataEntry(value: Double(rateDataSource[i]),
+									 label: "",
+									 icon: nil)
+		}
+		
+		let set = PieChartDataSet(entries: entries, label: "Results")
+		set.drawIconsEnabled = false
+		set.sliceSpace = 0
+		
+		set.setColor(Style.Color.backgroundTf)
+		set.addColor(Style.Color.mainGreen)
+		
+		let data = PieChartData(dataSet: set)
+		
+		let pFormatter = NumberFormatter()
+		pFormatter.numberStyle = .percent
+		pFormatter.maximumFractionDigits = 1
+		pFormatter.multiplier = 10
+		pFormatter.percentSymbol = " %"
+		data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
+		
+		data.setValueFont(.systemFont(ofSize: 11, weight: .light))
+		data.setValueTextColor(.black)
+		
+		chartView.data = data
+		chartView.highlightValues(nil)
+	}
+
 
 }
 
